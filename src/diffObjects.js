b2774d099cd074faec45117806b192becd4ebe1f
dydaev <dydaev@gmail.com>
2018-02-21 00:24:53 +0200

    fix small bugs

--- src/diffObjects.js
+++ src/diffObjects.js
@@ -5,24 +5,23 @@ export default function diffObjects(obA, obB) {
     updated: {},
     deleted: {},
   };
-	  
+
   (function walker(oA, oB, objectPath = []) {
     const oAB = Object.assign({}, oA, oB);
-    
+
     const path = (arrayKeys, deepObject = {}) =>
       arrayKeys.reduceRight((acc, key) => {
         return { [key]: { ...acc } };
       }, deepObject);
-      
+
     Object.keys(oAB).forEach(subAB => {
-      let saved = undefined;
-      let added = undefined;
-      let updated  = undefined;
-      let deleted  = undefined;
+      let saved;
+      let added;
+      let updated;
+      let deleted;
 
       if (typeof oA[subAB] === 'object' &&
-      typeof oB[subAB] === 'object' &&
-      subAB in oA && subAB in oB) {
+      typeof oB[subAB] === 'object') {
         walker(oA[subAB], oB[subAB], [...objectPath, subAB]);
       } else {
         if (!(subAB in oA) &&
@@ -35,34 +34,35 @@ export default function diffObjects(obA, obB) {
         if (subAB in oA &&
         subAB in oB &&
         oA[subAB] !== oB[subAB]) updated = oAB[subAB];
-        
+
         if (subAB in oA &&
         !(subAB in oB)) deleted = oAB[subAB];
-        
+
         result = {
           added: Object.assign(
             {},
             {...result.added},
-            added !== undefined ? path(objectPath ,{[subAB]: added}) : {}
+            added !== undefined ? path(objectPath, { [subAB]: added }) : {}
           ),
           saved: Object.assign(
             {},
             {...result.saved},
-            saved !== undefined ? path(objectPath ,{[subAB]: saved}) : {}
-            ),
+            saved !== undefined ? path(objectPath, { [subAB]: saved }) : {}
+          ),
           updated: Object.assign(
             {},
             {...result.updated},
-            updated !== undefined ? path(objectPath ,{[subAB]: updated}) : {}
+            updated !== undefined ? path(objectPath, { [subAB]: updated }) : {}
           ),
           deleted: Object.assign(
-          {},
-          {...result.deleted},
-            deleted !== undefined ? path(objectPath ,{[subAB]: deleted}) : {}
-        ),
-        }
+            {},
+            {...result.deleted},
+            deleted !== undefined ? path(objectPath, { [subAB]: deleted }) : {}
+          ),
+        };
       }
     });
   })(obA, obB)
-	return result;
+
+  return result;
 }
